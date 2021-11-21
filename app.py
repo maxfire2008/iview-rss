4cc8340561f83b6251cb8e35858c79f6ac6b57f3
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-11-21 11:30:06 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from honeybadger import honeybadger
+honeybadger.configure(api_key='hbp_lV19LQk4cRD0WUcO7cZ1sW2iS3hk8g1ZNec6')
+
 import flask
 import requests
 import json
+
+#honeybadger.notify(exc, context={'foo': 'bar'})
 
 app = flask.Flask(__name__)
 
@@ -16,6 +21,8 @@ def rss_show(show):
         ).content.decode()
     )
     if show_info['type'] == 'series':
-        
+        pass
+    else:
+        honeybadger.notify(f"{show_info['type']} is not recognised!", context=show)
     return show
 
